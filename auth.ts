fbf52c984c8f430ef5d527f303b013accc38fd83
zcyzcy98 <51557777+zcyzcy98@users.noreply.github.com>
2025-07-29 10:36:57 +0800

    update: auth权限

---
+++ auth.ts
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+import { prisma } from "./lib/prisma";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+  providers: [GitHub],
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.name = user.name;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+        session.user.name = token.name as string;
+      }
+      return session;
+    },
+  },
+});
