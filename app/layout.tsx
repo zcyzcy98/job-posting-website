f3cb869df10080a4ab9afddf578037822dd2942e
zcyzcy98 <51557777+zcyzcy98@users.noreply.github.com>
2025-07-30 10:59:42 +0800

    update: formate

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,10 +33,10 @@ export default async function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <SessionProvider session={session}>
-        <div className="min-h-screen bg-gray-50">
-          <Navbar />
-          <main className="container mx-auto px-4 py-8">{children}</main>
-        </div>
+          <div className="min-h-screen bg-gray-50">
+            <Navbar />
+            <main className="container mx-auto px-4 py-8">{children}</main>
+          </div>
         </SessionProvider>
       </body>
     </html>
