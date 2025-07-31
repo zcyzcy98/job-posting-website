09162a76ef1948e90944f26f9b64803f960ba342
zcyzcy98 <51557777+zcyzcy98@users.noreply.github.com>
2025-07-31 12:24:24 +0800

    update: createPost

---
+++ app/actions/post.action.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { auth } from "@/auth";
+import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
+import { NextResponse } from "next/server";
+
+export async function createPost(data: any) {
+  data = JSON.parse(JSON.stringify(data));
+  const session = await auth();
+
+  if (!session?.user || !session.user.id) {
+    return redirect("/auth/signin");
+  }
+
+  try {
+    const job = await prisma.job.create({
+      data: {
+        ...data,
+        postedById: session.user.id,
+      },
+    });
+    return JSON.parse(JSON.stringify(job));
+  } catch (error) {
+    console.error("Error creating job: ", error);
+    return new NextResponse("Internal server error", { status: 500 });
+  }
+}
